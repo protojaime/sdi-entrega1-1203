d51f17d219db9e7e1344344c39aebfdb948e5afe
protojaime <33036535+protojaime@users.noreply.github.com>
2019-03-13 12:34:34 +0100

    Product service & repostory created

---
+++ sdi-entrega1-1203/src/main/java/com/uniovi/repositories/ProductsRepository.java
@@ -0,0 +1,26 @@
+package com.uniovi.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+import com.uniovi.entities.Product;
+import com.uniovi.entities.User;
+public interface ProductsRepository extends CrudRepository<Product, Long>{
+	@Query("SELECT r FROM Product r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1))")
+			Page<Product> searchByDescriptionAndName(Pageable pageable, String seachtext);
+			@Query("SELECT r FROM Product r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1)) AND r.user = ?2 ")
+			Page<Product> searchByDescriptionNameAndUser(Pageable pageable, String seachtext, User user);
+			@Query("SELECT r FROM Product r WHERE r.user = ?1 ORDER BY r.id ASC ")
+			Page<Product> findAllByUser(Pageable pageable, User user);
+			Page<Product> findAll(Pageable pageable); 
+
+	@Modifying
+	@Transactional
+	@Query("UPDATE Product SET sold = ?1 WHERE id = ?2")
+	void updateResend(Boolean resend, Long id);
+
+}
---
+++ sdi-entrega1-1203/src/main/java/com/uniovi/services/ProductService.java
@@ -0,0 +1,104 @@
+package com.uniovi.services;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import com.uniovi.entities.*;
+import com.uniovi.repositories.ProductsRepository;
+import com.uniovi.services.ProductService;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private HttpSession httpSession;
+
+	@Autowired
+	private ProductsRepository ProductsRepository;
+
+	public List<Product> getProducts() {
+		
+		List<Product> Products = new ArrayList<Product>();
+		ProductsRepository.findAll().forEach(Products::add);
+		return Products;
+	}
+	
+	public void setProductResend(boolean revised,Long id){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		Product Product = ProductsRepository.findById(id).get();
+		if(Product.getUser().getDni().equals(dni) ) {
+		ProductsRepository.updateResend(revised, id);
+		}
+	}
+
+	
+	
+	@SuppressWarnings("unchecked")
+	public Product getProduct(Long id) {
+		Set<Product> consultedList = (Set<Product>) httpSession.getAttribute("consultedList");
+		if ( consultedList == null ) {
+		consultedList = new HashSet<Product>();
+		}
+		Product ProductObtained = ProductsRepository.findById(id).get();
+		consultedList.add(ProductObtained);
+		httpSession.setAttribute("consultedList"
+		, consultedList);
+		return ProductObtained;
+	}
+
+
+	
+	
+	
+	public Page<Product> getProductsForUser(Pageable pageable, User user){
+		Page<Product> Products = new PageImpl<Product>(new LinkedList<Product>());
+		if ( user.getRole().equals("ROLE_USER")) {
+		Products = ProductsRepository.findAllByUser(pageable, user);
+		} 
+		if ( user.getRole().equals("ROLE_ADMIN")){
+		Products = getProducts(pageable);
+		}
+		return Products;
+		}
+		public Page<Product> searchProductsByDescriptionAndNameForUser (Pageable pageable, 
+		String searchText, User user){
+		Page<Product> Products = new PageImpl<Product>(new LinkedList<Product>());
+		searchText = "%"+searchText+"%";
+		if ( user.getRole().equals("ROLE_USER")) {
+		Products = ProductsRepository.searchByDescriptionNameAndUser(pageable, searchText, user);
+		} 
+		if ( user.getRole().equals("ROLE_ADMIN")){
+		Products = ProductsRepository.searchByDescriptionAndName(pageable, searchText);
+		}
+		return Products;
+		}
+		public Page<Product> getProducts(Pageable pageable){
+		Page<Product> Products = ProductsRepository.findAll(pageable);
+		return Products;
+		}
+
+	
+	
+	public void addProduct(Product Product) {
+// Si en Id es null le asignamos el ultimo + 1 de la lista
+		ProductsRepository.save(Product);
+	}
+
+	public void deleteProduct(Long id) {
+		ProductsRepository.deleteById(id);
+	}
+}
