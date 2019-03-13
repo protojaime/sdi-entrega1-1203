857fc5f3f9d0a1e0e618e7938ff2b633bf780d82
protojaime <33036535+protojaime@users.noreply.github.com>
2019-03-13 12:20:59 +0100

    Product.java created, based in mark.java

---
+++ sdi-entrega1-1203/src/main/java/com/uniovi/entities/Product.java
@@ -0,0 +1,92 @@
+package com.uniovi.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String description;
+	private Double price;
+	private Boolean sold;
+	
+	public Boolean getSold() {
+		return sold;
+	}
+
+	public void setSold(Boolean sold) {
+		this.sold = sold;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+
+
+	
+	public Product(Long id, String description, Double price) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Product(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.price = score;
+		this.user = user;
+	}
+
+	public Product() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getScore() {
+		return price;
+	}
+
+	public void setScore(Double score) {
+		this.price = score;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	
+	
+}
